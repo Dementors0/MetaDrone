b65dbcee0b766025a36a787628a93b8f2f458f12
snz <1240451437@qq.com>
2026-03-17 18:46:35 +0800

    去掉smooth损失

--- mmgj_transformer.py
+++ mmgj_transformer.py
@@ -1,8 +1,4 @@
-#5.5.2  在物理反馈仿真基础  INFRA
-#       + 单独画四个代理损失对 Worker 梯度的 norm
-#       + 碰撞可视化
-#       + 环境渲染的原始深度图可视化优化
-#       + 无人机旋转rpy姿态可视化
+#5.5.2.1  去掉smooth损失
 import argparse
 import math
 from collections import defaultdict
